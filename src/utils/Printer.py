d2345c622d32616f8424f1ee9608cb219b5fcb8b
akmaldika <13521070@mahasiswa.itb.ac.id>
2023-12-01 15:50:21 +0700

    feat: titile

--- src/utils/Printer.py
+++ src/utils/Printer.py
@@ -1,4 +1,5 @@
 from model.Operation import Operation
+import shutil
 
 
 # Define widths for each part of the output
@@ -6,6 +7,14 @@ op_type_width = 15
 data_item_name_width = 15  
 transaction_id_width = 15  
 
+
+def print_title(text):
+    terminal_width, _ = shutil.get_terminal_size()
+    line = "-" * terminal_width
+    padding = (terminal_width - len(text)) // 2
+    print(line)
+    print(line[:padding] + text + line[padding + len(text):])
+    print(line)
 
 
 def print_process(op_type: str, transaction_id: str, data_item_name: str, descriptions: list[str]):
