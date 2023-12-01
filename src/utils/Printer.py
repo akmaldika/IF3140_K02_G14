02bd0a2f2fec41b6190d9d3a8629f24f3d2b6114
melvinkj <melvinkentj@gmail.com>
2023-12-01 12:34:35 +0700

    feat: add formatted printer

---
+++ src/utils/Printer.py
@@ -0,0 +1,42 @@
+from model.Operation import Operation
+
+
+# Define widths for each part of the output
+op_type_width = 15 
+data_item_name_width = 15  
+transaction_id_width = 15  
+
+
+
+def print_process(op_type: str, transaction_id: str, data_item_name: str, descriptions: list[str]):
+    op_type_mapping = {
+        "R": "Read",
+        "W": "Write",
+        "C": "Commit",
+        "A": "Abort",
+        "XL": "Grant X-Lock",
+        "SL": "Grant S-Lock",
+        "UL": "Unlock",
+        "TW": "Temp Write"
+    }
+    op_type = op_type_mapping.get(op_type, op_type)
+
+    description_str = ' | '.join(descriptions)
+
+    # Format the output with specified widths
+    output = f"{op_type.ljust(op_type_width)}{data_item_name.ljust(data_item_name_width)}T{transaction_id.ljust(transaction_id_width)}{description_str}"
+
+    print(output)
+
+    
+def print_header():
+    # Format the header with specified widths
+    header = f"{'Operation'.ljust(op_type_width)}{'Data Item'.ljust(data_item_name_width)}{'Transaction'.ljust(transaction_id_width)} Description"
+    print()
+    print(header)
+
+
+def print_result(result: list[Operation]):
+    print("Result: ")
+    for i in result:
+      print(i.getNameString() + "; ", end="")
